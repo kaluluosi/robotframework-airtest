833dfbbe39c978eb3583478b58e0ed1ee4e43988
kaluluosi <kaluluosi@gmail.com>
2023-10-21 02:44:58 +0800

    提交配置

--- robotframework_airtest/vmg/settings.py
+++ robotframework_airtest/vmg/settings.py
@@ -1,16 +1,19 @@
 import toml
 
 from pydantic import BaseModel, RootModel, Field
+from typing import Dict
 
 
 class Setting(BaseModel):
     source: str = Field(default="views", description="你的前端界面资源文件存放目录")
     dist: str = Field(default="view_models", description="你的界面模型资源输出目录")
-    exts: str = Field(default="unity", description="界面资源文件格式")
+    exts: str = Field(default=".prefab", description="界面资源文件格式")
     generator: str = Field(default="unity", description="界面模型导出器")
 
 
-class Settings(RootModel[dict]):
+class Settings(RootModel[Dict[str, Setting]]):
+    root: Dict[str, Setting] = {}
+
     @classmethod
     def load(cls, file: str = "vmg.toml"):
         data = toml.load(file)
