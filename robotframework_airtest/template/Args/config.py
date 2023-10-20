3ffadc8b3a467231e0e14b3073406558883d9e02
kaluluosi <kaluluosi@gmail.com>
2023-10-21 03:53:49 +0800

    修正模板

--- robotframework_airtest/template/Args/config.py
+++ robotframework_airtest/template/Args/config.py
@@ -1,9 +1,9 @@
 import json
 
-with open(".vscode/settings.json", "r") as f:
+with open(".vscode/settings.json", "r", encoding="utf-8") as f:
     args = json.load(f)
-
-pkg_name = args["pkg_name"] # pc客户端启动程序
-device_uri = args["device_uri"]   # 设备连接字符串
+args = args["robot.variables"]
+pkg_name = args["pkg_name"]  # pc客户端启动程序
+device_uri = args["device_uri"]  # 设备连接字符串
 auto_start_app = args["auto_start_app"]  # 自动开启app
 serverid = args["serverid"]  # 登录的服务器id
