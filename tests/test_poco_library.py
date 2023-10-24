60156496adcaf1dda1c9e899d2ddc19661a691c6
kaluluosi111 <kaluluosi@gmail.com>
2023-10-24 18:38:05 +0800

    增加几个PocoLibrary

--- tests/test_poco_library.py
+++ tests/test_poco_library.py
@@ -7,25 +7,3 @@ class TestStdPoco(unittest.TestCase):
         result = parse_url_to_queryinfos("btn_start")
 
         self.assertTrue(result)
-
-    def test_guest(self):
-        from poco.drivers.unity3d import UnityPoco
-        from airtest.core.api import connect_device
-
-        connect_device("android:///")
-
-        poco = UnityPoco()
-
-        star = poco("star")
-        shell = poco("shell")
-
-        star_pos = star.get_position()
-        shell_pos = shell.get_position()
-
-        poco.device.keyevent(star_pos[0], star_pos[1])
-        poco.device.key_press("LEFT")
-
-        poco.device.mouse_down(shell_pos[0], shell_pos[1])
-        poco.device.key_release("LEFT")
-
-        poco.start_gesture
